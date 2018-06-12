98452970410b6916c0fb7b80707b6318350eae5d
Dimitriweb92 <moysondimitri@gmail.com>
2018-06-12 16:31:35 +0200

    form uptdate

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -30,15 +30,15 @@ if(isset($_GET['idcontenu'])&&is_numeric($_GET['idcontenu'])) {
 /*
  * on veut insérer un nouvel article
  */
-}elseif (isset($_GET['insert'])){
+}elseif (isset($_GET['insert'])) {
 
     // Si formulaire non envoyé
-    if(empty($_POST)){
+    if (empty($_POST)) {
 
         // appel de la vue
         require_once "View/formInsert.html.php";
 
-    }else{
+    } else {
 
         // hydratation d'un objet Contenu avec les variables POST
         $newContenu = new Contenu($_POST);
@@ -46,15 +46,52 @@ if(isset($_GET['idcontenu'])&&is_numeric($_GET['idcontenu'])) {
         // insertion dans la table contenu d'un objet de type Contenu
         $result = $ContenuManager->create($newContenu);
 
-        if($result){
+        if ($result) {
             header("Location: ./");
-        }else{
+        } else {
             $erreur = "Veuillez recommencer";
             require_once "View/formInsert.html.php";
         }
 
     }
 
+/*
+* on veut modifier un article via son id
+*/
+}elseif (isset($_GET['update'])&&is_numeric($_GET['update'])){
+
+    $id = (int) $_GET['update'];
+
+    // on récupère l'article que l'on veut modifier
+    $rempliForm = $ContenuManager->getContenuById($id);
+
+    // on le transforme en objet
+    $recup = new Contenu($rempliForm);
+
+    // Si formulaire non envoyé
+    if (empty($_POST)) {
+
+        // si on essaye de modifier un article qui n'existe pas/plus
+        if(!$rempliForm) die("Vous essayez de modifier un article qui n'existe pas/plus");
+
+        // appel de la vue
+        require_once "View/formUpdate.html.php";
+
+    } else {
+
+        // on le transforme en objet
+        $recup = new Contenu($_POST);
+
+        // mise à jour de l'article
+        $modif = $ContenuManager->update($recup);
+
+        if($modif){
+            header("Location: ./?idcontenu={$recup->getIdcontenu()}");
+        }else{
+            $erreur = "Veuillez recommencer";
+            require_once "View/formUpdate.html.php";
+        }
+    }
 
 /*
  * accueil
